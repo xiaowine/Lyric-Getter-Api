847e9dfa2df8c4415ec5afea6f9379abdd5d24de
xiaowine <xiaowine0@gmail.com>
2023-06-18 23:47:37 +0800

    fix android.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {

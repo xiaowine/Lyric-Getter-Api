6bc0905336bfa5fd80ab22c2186b0cea026ad1f0
xiaowine <xiaowine0@gmail.com>
2023-07-14 09:48:47 +0800

    优化代码

--- src/main/java/cn/lyric/getter/api/tools/Tools.kt
+++ src/main/java/cn/lyric/getter/api/tools/Tools.kt
@@ -5,13 +5,32 @@ import android.content.Context
 import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.VectorDrawable
 import android.os.Build
 import android.util.Base64
+import android.util.Log
 import cn.lyric.getter.api.LyricReceiver
 import cn.lyric.getter.api.data.LyricData
 import java.io.ByteArrayOutputStream
 
+
 object Tools {
+
+    private fun makeDrawableToBitmap(drawable: Drawable): Bitmap {
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.apply {
+            setBounds(0, 0, canvas.width, canvas.height)
+            draw(canvas)
+        }
+        return bitmap
+    }
+
+
     /**
      *
      * @param [base64] 图片的Base64
@@ -22,8 +41,38 @@ object Tools {
             val bitmapArray: ByteArray = Base64.decode(base64, Base64.DEFAULT)
             BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.size)
         } catch (e: Exception) {
-            e.printStackTrace()
             null
+        }
+    }
+
+    /**
+     * 将Drawable转换成Base64
+     *
+     * @param drawable 图片
+     * @return [String] 返回图片的Base64
+     */
+    fun drawableToBase64(drawable: Drawable): String {
+        Log.d("aaaaaa",drawable::class.java.name)
+        when (drawable) {
+            is BitmapDrawable -> {
+                return drawableToBase64(drawable.bitmap)
+            }
+
+            is VectorDrawable -> {
+                return drawableToBase64(makeDrawableToBitmap(drawable))
+            }
+
+            is AdaptiveIconDrawable -> {
+                return drawableToBase64(makeDrawableToBitmap(drawable))
+            }
+
+            else -> {
+                return try {
+                    drawableToBase64((drawable as BitmapDrawable).bitmap)
+                } catch (_: Exception) {
+                    ""
+                }
+            }
         }
     }
 
@@ -40,6 +89,7 @@ object Tools {
         val bytes = stream.toByteArray()
         return Base64.encodeToString(bytes, Base64.DEFAULT)
     }
+
     /**
      * 接待抒情
      * @param [context] Context
